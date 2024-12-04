8d72b5a0456a0a4f89ed3ebf0f352f8a2cb73f00
Harrison Doran <harrydoran@pm.me>
2024-12-04 16:26:51 +1300

    Print input from file or interactive prompt

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Glox: Another Lox compiler
+# Glox: Another Lox interpreter
 
-Glox is a compiler for the programming language Lox, written in Go.
+Glox is an interpreter for the programming language Lox, written in Go.
 
 Built following ["Crafting Interpreters" by Robert Nystrom](https://craftinginterpreters.com/).
--- main.go
+++ main.go
@@ -1,9 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 )
 
+func run(source string) {
+	fmt.Println(source)
+}
+
+func runFile(path string) {
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	run(string(bytes))
+}
+
+func runPrompt() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Printf("> ")
+	for scanner.Scan() {
+		err := scanner.Err()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		line := scanner.Text()
+		run(line)
+		fmt.Printf("> ")
+	}
+}
+
+func error(line int, message string) {
+	report(line, "", message)
+}
+
+func report(line int, where string, message string) {
+	fmt.Printf("[line %v] Error %v: %v", line, where, message)
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	if len(os.Args) > 2 {
+		fmt.Println("Hello, World!")
+		os.Exit(64)
+	} else if len(os.Args) == 2 {
+		runFile(os.Args[0])
+	} else {
+		runPrompt()
+	}
 }
